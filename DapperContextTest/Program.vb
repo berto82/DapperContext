5c0af0b4d006d6b6061e2bd0422e754ff34f9eca
Roberto Faccani <rfaccani@ncweb.it>
2025-05-13 10:48:39 +0200

    Aggiorna il progetto BertoSoftware con nuove funzionalità
    Modificato il `RootNamespace` in `DapperContextTest.vbproj` e aggiunta la versione `0.1.0.1`. Inclusi riferimenti al progetto `DapperAuditContext` e configurazione per `appsettings.json`.
    
    Ristrutturata la classe `Person` in `Person.vb` con annotazioni per il database e proprietà aggiornate.
    
    Implementata la logica per l'inserimento di un oggetto `Person` in `Program.vb` utilizzando `DapperAuditContext`.
    
    Aggiunta stringa di connessione al database in `appsettings.json`.

--- DapperContextTest/Program.vb
+++ DapperContextTest/Program.vb
@@ -3,6 +3,18 @@ Imports System
 Module Program
     Sub Main(args As String())
 
+        Using ctx As New DapperAuditContext
+
+            Dim person As New Model.Person With {
+                .Name = "John",
+                .Surname = "Doe"
+            }
+
+            ctx.InsertOrUpdate(person)
+
+
+        End Using
+
 
     End Sub
 End Module
