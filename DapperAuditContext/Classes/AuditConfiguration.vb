1d23866e7126ad09d1177816282d53eb93275591
Roberto Faccani <r.faccani@outlook.it>
2025-05-16 10:18:44 +0200

    Aggiunto classi per configurazione

--- DapperAuditContext/Classes/AuditConfiguration.vb
+++ DapperAuditContext/Classes/AuditConfiguration.vb
@@ -1,18 +1,37 @@
 ﻿Public Class AuditConfiguration
+    Implements IAuditConfigurationBuilder
 
-    Enum AuditStoreMode
-        Database
-        File
-    End Enum
-
-    Public ReadOnly Property StoreMode As AuditStoreMode
+    Public ReadOnly Property StoreLogMode As AuditStoreMode
     Public ReadOnly Property FilePath As String
     Public ReadOnly Property TableName As String
 
-    Friend Sub New(logStoreMode As AuditStoreMode, logfilePath As String, logTableName As String)
-        _StoreMode = logStoreMode
-        _FilePath = logfilePath
-        _TableName = logTableName
+    Private Sub New()
+        _StoreLogMode = AuditStoreMode.Database
+        _TableName = "AuditTable"
+        _FilePath = $"audit_{Date.UtcNow:yyyyMMdd}.log"
     End Sub
+
+    Public Shared Function CreateNew() As IAuditConfigurationBuilder
+        Return New AuditConfiguration
+    End Function
+
+    Public Function StoreMode(mode As AuditStoreMode) As IAuditConfigurationBuilder Implements IAuditConfigurationBuilder.StoreMode
+        _StoreLogMode = mode
+        Return Me
+    End Function
+
+    Public Function WithCustomLogFilename(path As String) As IAuditConfigurationBuilder Implements IAuditConfigurationBuilder.WithCustomLogFilename
+        _FilePath = path
+        Return Me
+    End Function
+
+    Public Function WithCustomTableName(tablename As String) As IAuditConfigurationBuilder Implements IAuditConfigurationBuilder.WithCustomTableName
+        _TableName = tablename
+        Return Me
+    End Function
+
+    Public Function Build() As AuditConfiguration Implements IAuditConfigurationBuilder.Build
+        Return Me
+    End Function
 
 End Class
--- DapperAuditContext/Classes/AuditDelta.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class AuditDelta
-
-    Public Property FieldName As String
-    Public Property ValueBefore As String
-    Public Property ValueAfter As String
-End Class
