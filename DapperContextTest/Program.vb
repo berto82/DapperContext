8073bcbbc652a0606d6649539464e9827f9e717a
Roberto Faccani <r.faccani@outlook.it>
2025-05-18 01:40:03 +0200

    Update test program

--- DapperContextTest/Program.vb
+++ DapperContextTest/Program.vb
@@ -3,30 +3,38 @@ Imports System
 Module Program
     Sub Main(args As String())
 
-        DapperAuditContext.AuditSettings = AuditConfiguration.CreateNew.StoreMode(AuditStoreMode.File).Build
+        Try
+            DapperContext.Settings = ContextConfiguration.CreateNew.UseSettingsFileMode(SettingFileMode.NET4x).WithConnectionName("pippo").Build
+            DapperAuditContext.AuditSettings = AuditConfiguration.CreateNew.StoreMode(AuditStoreMode.File).Build
 
-        Dim dbName As String = New DapperAuditContext().Connection.Database
-        Dim dbExist As Boolean = New DapperAuditContext().DatabaseExist(dbName)
+            Dim dbExist As Boolean = New DapperContext().DatabaseExist
+
+            'Add new value into Person Table with an automatic audit trail, just use DapperAuditContext instead of DapperContext
+            Using ctx As New DapperAuditContext
 
 
-        'Add new value into Person Table with an automatic audit trail, just use DapperAuditContext instead of DapperContext
-        Using ctx As New DapperAuditContext
+                Dim person As New Model.Person With {
+                    .Name = "John",
+                    .Surname = "Doe"
+                }
+
+                ctx.InsertOrUpdate(person)
+
+                'Changes a property after save
+                person.Surname = "Black"
+
+                'Saves the changes and adds a record in the control table with the difference between the previous save.
+                ctx.InsertOrUpdate(person)
+
+            End Using
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
 
 
-            Dim person As New Model.Person With {
-                .Name = "John",
-                .Surname = "Doe"
-            }
 
-            ctx.InsertOrUpdate(person)
 
-            'Changes a property after save
-            person.Surname = "Black"
-
-            'Saves the changes and adds a record in the control table with the difference between the previous save.
-            ctx.InsertOrUpdate(person)
-
-        End Using
 
     End Sub
 End Module
