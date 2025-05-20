d1b70b01e5580d91efefad307fef27195717f67c
Roberto Faccani <r.faccani@outlook.it>
2025-05-20 07:34:36 +0200

    Update test program

--- DapperContextTest/Program.vb
+++ DapperContextTest/Program.vb
@@ -1,40 +1,154 @@
 Imports System
+Imports System.IO
 
 Module Program
     Sub Main(args As String())
 
         Try
-            DapperContext.Settings = ContextConfiguration.CreateNew.UseSettingsFileMode(SettingFileMode.NETCore).Build
-            '     DapperAuditContext.AuditSettings = AuditConfiguration.CreateNew.StoreMode(AuditStoreMode.File).Build
+            '   ExampleNotAuditing()
 
-            Dim dbExist As Boolean = New DapperContextSqlServer().DatabaseExist
-
-            'Add new value into Person Table with an automatic audit trail, just use DapperAuditContext instead of DapperContext
-            Using ctx As New DapperAuditContextSqlServer
-
-
-                Dim person As New Model.Person With {
-                    .Name = "John",
-                    .Surname = "Doe"
-                }
-
-                ctx.InsertOrUpdate(person)
-
-                'Changes a property after save
-                person.Surname = "Black"
-
-                'Saves the changes and adds a record in the control table with the difference between the previous save.
-                ctx.InsertOrUpdate(person)
-
-            End Using
+            ExampleWithAuditing()
 
         Catch ex As Exception
             Console.WriteLine(ex.Message)
         End Try
 
+    End Sub
 
 
+    Sub ExampleNotAuditing()
 
+        'Configure context setting or leave default
+        '***Uncomment this line if you want to configure settings
+        'DapperContext.Settings = ContextConfiguration.CreateNew.UseSettingsFileMode(SettingFileMode.NETCore).Build
+
+        'Create a record
+        Using ctx As New DapperContextSqlServer
+
+            Dim person As New Model.Person With {
+                .Name = "John",
+                .Surname = "Doe"
+            }
+
+            ctx.InsertOrUpdate(person)
+
+        End Using
+
+        'Get a single record
+        Using ctx As New DapperContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            Console.WriteLine(String.Join(" | ", {person.ID, person.Name, person.Surname}))
+
+        End Using
+
+        'Get all record
+        Using ctx As New DapperContextSqlServer
+
+            Dim lstPerson As List(Of Model.Person) = ctx.GetAll(Of Model.Person).ToList
+
+            lstPerson.ForEach(Sub(x) Console.WriteLine(String.Join(" | ", {x.ID, x.Name, x.Surname})))
+
+        End Using
+
+        'Update a record
+        Using ctx As New DapperContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            person.Surname = "Butt"
+
+            ctx.InsertOrUpdate(person)
+
+            Console.WriteLine(String.Join(" | ", {person.ID, person.Name, person.Surname}))
+
+        End Using
+
+        'Delete a record
+        Using ctx As New DapperContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            ctx.Delete(person)
+        End Using
+
+        'Delete all record
+        Using ctx As New DapperContextSqlServer
+            ctx.DeleteAll(Of Model.Person)()
+        End Using
 
     End Sub
+
+    Sub ExampleWithAuditing()
+
+        'Configure context audit setting or leave default
+        '***Uncomment this line if you want to configure settings
+        ' DapperAuditContext.AuditSettings = AuditConfiguration.CreateNew.StoreMode(AuditStoreMode.File).Build
+
+        'Create a record
+        Using ctx As New DapperAuditContextSqlServer
+
+            Dim person As New Model.Person With {
+                .Name = "John",
+                .Surname = "Doe"
+            }
+
+            ctx.InsertOrUpdate(person)
+
+        End Using
+
+        'Get a single record
+        Using ctx As New DapperAuditContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            Console.WriteLine(String.Join(" | ", {person.ID, person.Name, person.Surname}))
+
+        End Using
+
+        'Get all record
+        Using ctx As New DapperAuditContextSqlServer
+
+            Dim lstPerson As List(Of Model.Person) = ctx.GetAll(Of Model.Person).ToList
+
+            lstPerson.ForEach(Sub(x) Console.WriteLine(String.Join(" | ", {x.ID, x.Name, x.Surname})))
+
+        End Using
+
+        'Update a record
+        Using ctx As New DapperAuditContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            person.Surname = $"{person.Surname}_{Date.Now.Ticks}"
+
+            ctx.InsertOrUpdate(person)
+
+            Console.WriteLine(String.Join(" | ", {person.ID, person.Name, person.Surname}))
+
+        End Using
+
+        'Delete a record
+        Using ctx As New DapperAuditContextSqlServer
+
+            Dim person As Model.Person = ctx.Get(Of Model.Person)(1)
+
+            ctx.Delete(person)
+        End Using
+
+        'Delete all record
+        Using ctx As New DapperAuditContextSqlServer
+            ctx.DeleteAll(Of Model.Person)()
+        End Using
+
+        'Show audit record
+        Dim logDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AuditLogs")
+        Dim logFile As String = Path.Combine(logDir, DapperAuditContext.AuditSettings.FilePath)
+        Dim auditLog As String = IO.File.ReadAllText(logFile)
+        Console.Write(auditLog)
+
+    End Sub
+
+
 End Module
