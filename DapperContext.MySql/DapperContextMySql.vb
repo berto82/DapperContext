6c9dfc181aa9821fe394d6ce617046bb3cdd083f
Roberto Faccani <r.faccani@outlook.it>
2025-05-20 07:34:22 +0200

    Add new project for MySql provider

---
+++ DapperContext.MySql/DapperContextMySql.vb
@@ -0,0 +1,51 @@
+Imports System.Data
+Imports Dapper
+Imports MySqlConnector
+
+Public Class DapperContextMySql
+    Inherits DapperContext
+
+    Public Sub New()
+
+        Try
+            Dim cnString As String = GetConnectionString()
+
+            Dim cnStringBuilder As New MySqlConnectionStringBuilder(cnString)
+
+            Me.Connection = New MySqlConnection(cnString)
+            Me.Connection.ConnectionString = cnStringBuilder.ConnectionString
+            Me.Connection.Open()
+
+        Catch ex As Exception
+            Throw
+        End Try
+    End Sub
+
+    Public Overrides Function DatabaseExist(dbName As String) As Boolean
+
+        Dim result As String
+
+        If Settings.EnableTransaction = True Then
+            Using transaction As IDbTransaction = Me.Connection.BeginTransaction
+                Try
+                    result = Me.Connection.Query(Of String)("SELECT schema_name FROM infomrmation_schema.schemata WHERE schema_name = @p0", New With {.p0 = dbName}, transaction).FirstOrDefault
+                    transaction.Commit()
+
+                    Return result <> String.Empty
+                Catch ex As Exception
+                    transaction.Rollback()
+                    Throw
+                End Try
+            End Using
+        Else
+            Try
+                result = Me.Connection.Query(Of String)("SELECT schema_name FROM infomrmation_schema.schemata WHERE schema_name = @p0", New With {.p0 = dbName}).FirstOrDefault
+            Catch ex As Exception
+                Throw
+            End Try
+        End If
+
+        Return False
+
+    End Function
+End Class
