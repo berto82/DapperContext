5cf648aeda450c54f5846168fe0a26aa3174380f
Roberto Faccani <r.faccani@outlook.it>
2025-05-26 00:14:51 +0200

    Fix SQL script table for SQL Server

--- DapperAuditContext.SqlServer/DapperAuditContextSqlServer.vb
+++ DapperAuditContext.SqlServer/DapperAuditContextSqlServer.vb
@@ -2,85 +2,89 @@ Imports System.Data
 Imports System.Data.Common
 Imports System.IO
 Imports System.Reflection
+Imports BertoSoftware.Context.Configuration
 Imports Dapper
 Imports Microsoft.Data.SqlClient
 
-Public Class DapperAuditContextSqlServer
-    Inherits DapperAuditContext
+Namespace Context.Tools.Audit
 
-    Public Sub New()
+    Public Class DapperAuditContextSqlServer
+        Inherits DapperAuditContext
 
-        Try
-            Dim cnString As String = GetConnectionString()
+        Public Sub New()
 
-            Dim cnStringBuilder As New DbConnectionStringBuilder
-            cnStringBuilder.ConnectionString = cnString
+            Try
+                Dim cnString As String = GetConnectionString()
 
-            Dim efConnectionString As Object = Nothing
+                Dim cnStringBuilder As New DbConnectionStringBuilder
+                cnStringBuilder.ConnectionString = cnString
 
-            If cnStringBuilder.TryGetValue("provider connection string", efConnectionString) = True Then
-                cnStringBuilder.ConnectionString = CStr(efConnectionString)
-            End If
+                Dim efConnectionString As Object = Nothing
 
-            If cnStringBuilder.ContainsKey("TrustServerCertificate") = False Then
-                cnStringBuilder.Add("TrustServerCertificate", True)
-            End If
-
-            Me.Connection = New SqlConnection
-            Me.Connection.ConnectionString = cnStringBuilder.ConnectionString
-
-            Me.Connect()
-
-
-            If AuditSettings.StoreLogMode = AuditStoreMode.Database Then
-                Dim result = Query("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='AuditTable'")
-
-                If result.Count = 0 Then
-                    Dim scriptCreateDB As String = GetFromResources("AuditTable.sql")
-                    Execute(scriptCreateDB)
+                If cnStringBuilder.TryGetValue("provider connection string", efConnectionString) = True Then
+                    cnStringBuilder.ConnectionString = CStr(efConnectionString)
                 End If
 
-            End If
+                If cnStringBuilder.ContainsKey("TrustServerCertificate") = False Then
+                    cnStringBuilder.Add("TrustServerCertificate", True)
+                End If
 
-        Catch ex As Exception
-            Throw
-        End Try
+                Me.Connection = New SqlConnection
+                Me.Connection.ConnectionString = cnStringBuilder.ConnectionString
 
-    End Sub
+                Me.Connect()
 
-    Public Overrides Function DatabaseExist(dbName As String) As Boolean
+                If AuditSettings.StoreLogMode = AuditStoreMode.Database Then
+                    Dim result = Query("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME= @p0", New With {.p0 = AuditSettings.TableName})
 
-        Dim result As String
+                    If result.Count = 0 Then
+                        Dim scriptCreateDB As String = GetFromResources("AuditTable.sql")
+                        Execute(scriptCreateDB)
+                    End If
 
-        If Settings.EnableTransaction = True Then
-            Using transaction As IDbTransaction = Me.Connection.BeginTransaction
-                Try
-                    result = Me.Connection.Query(Of String)("SELECT name FROM master.sys.databases WHERE name = @p0", New With {.p0 = dbName}, transaction).FirstOrDefault
-                    transaction.Commit()
+                End If
 
-                    Return result <> String.Empty
-                Catch ex As Exception
-                    transaction.Rollback()
-                    Throw
-                End Try
-            End Using
-        Else
-            Try
-                result = Me.Connection.Query(Of String)("SELECT name FROM master.sys.databases WHERE name = @p0", New With {.p0 = dbName}).FirstOrDefault
             Catch ex As Exception
                 Throw
             End Try
-        End If
 
-        Return False
+        End Sub
 
-    End Function
+        Public Overrides Function DatabaseExist(dbName As String) As Boolean
 
-    Private Function GetFromResources(resourceName As String) As String
-        Using s As Stream = Assembly.GetExecutingAssembly.GetManifestResourceStream($"{[GetType].Namespace}.{resourceName}")
-            Using reader As New StreamReader(s)
-                Return reader.ReadToEnd
+            Dim result As String
+
+            If Settings.EnableTransaction = True Then
+                Using transaction As IDbTransaction = Me.Connection.BeginTransaction
+                    Try
+                        result = Me.Connection.Query(Of String)("SELECT name FROM master.sys.databases WHERE name = @p0", New With {.p0 = dbName}, transaction).FirstOrDefault
+                        transaction.Commit()
+
+                        Return result <> String.Empty
+                    Catch ex As Exception
+                        transaction.Rollback()
+                        Throw
+                    End Try
+                End Using
+            Else
+                Try
+                    result = Me.Connection.Query(Of String)("SELECT name FROM master.sys.databases WHERE name = @p0", New With {.p0 = dbName}).FirstOrDefault
+                Catch ex As Exception
+                    Throw
+                End Try
+            End If
+
+            Return False
+
+        End Function
+
+        Private Function GetFromResources(resourceName As String) As String
+            Using s As Stream = Assembly.GetExecutingAssembly.GetManifestResourceStream($"{[GetType].Namespace.Split("."c)(0)}.{resourceName}")
+                Using reader As New StreamReader(s)
+                    Return reader.ReadToEnd
+                End Using
             End Using
-        End Using
-    End Function
-End Class
+        End Function
+    End Class
+
+End Namespace
